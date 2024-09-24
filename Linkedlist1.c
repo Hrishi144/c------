6ca234ec728bf8dd0140d5d7905b87e8f52a8e8f
hrishicote <166132248+Hrishi144@users.noreply.github.com>
2024-09-24 19:08:16 +0530

    Update Linkedlist1.c

--- Linkedlist1.c
+++ Linkedlist1.c
@@ -3,16 +3,17 @@
 struct node{
     int data;
     struct node*link;
-}*temp,*head=NULL;
-void insertion();
-void insertion(){
-    int item;
+}*temp,*head=NULL,*ptr;
+int item;
+void insertion_at_begining();
+void insertion_at_end();
+void insertion_at_begining(){
     temp=(struct node*)malloc(sizeof(struct node));
     if(temp==NULL){
         printf("overflow");
-    } if(head==NULL){
-        printf("enter the data to be inserted");
+    } printf("enter the data to be inserted ");
         scanf("%d",&item);
+     if(head==NULL){
         temp->link=NULL;
         temp->data=item;
         head=temp;
@@ -20,24 +21,48 @@ void insertion(){
         printf("->%p\n",temp->link);
     
     }else{
-         printf("enter the data to be inserted");
-        scanf("%d",&item);
         temp->link=head;
         temp->data=item;
         head=temp;
         printf("%d",temp->data);
         printf("->%p\n",head->link);
     }
-} void main(){
+} void insertion_at_end(){
+      temp=(struct node*)malloc(sizeof(struct node));
+    if(temp==NULL){
+        printf("overflow");
+    }printf("enter the data to be inserted at end");
+        scanf("%d",&item);
+     if(head==NULL){
+        temp->link=NULL;
+        temp->data=item;
+        head=temp;
+        printf("%d",temp->data);
+        printf("->%p\n",temp->link);
+    }else{
+        ptr=head;
+         while(ptr->link!=NULL){
+            ptr=ptr->link;
+        }
+         temp->link=NULL;
+        temp->data=item;
+        ptr->link=temp;
+        printf("%d",temp->data);
+        printf("->%p\n",temp->link);
+    }
+}
+ void main(){
     int x;
     do{
         printf("\nenter your option\n");
         scanf("%d",&x);
         switch(x){
-            case 1:insertion();
+            case 1:insertion_at_begining();
             break;
-            case 2:break;
+            case 2: insertion_at_end();
+            break;
+            case 3:break;
             default:printf("enter a valid option\n");
         }
-    } while(x!=2);
+    } while(x!=3);
 }
